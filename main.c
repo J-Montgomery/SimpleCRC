5daa28c00c106ae73ccbefbdaf6127ab7ec3e6cc
Jordan Montgomery <montytyper@msn.com>
2021-03-24 13:23:26 -0700

    Working IBM-3740 CRC.

--- main.c
+++ main.c
@@ -14,6 +14,8 @@ struct crc16 {
     uint16_t check;
 };
 
+#define xstr(a) str(a)
+#define str(a) #a
 #define BITMASK(X) (1L << (X))
 #define CRC16_BITS (16)
 #define DECLARE_CRC16(NAME, POLY, INIT, REF_IN, REF_OUT, XOR_OUT, RESIDUE, CHECK)   \
@@ -21,29 +23,14 @@ struct crc16 {
                                    .ref_in = REF_IN, .ref_out = REF_OUT,     \
                                    .xor_out = XOR_OUT, .residue = RESIDUE, .check = CHECK }
 
-//DECLARE_CRC16(KERMIT, 0x1021, 0x0, true, false, 0x0, 0x0);
-DECLARE_CRC16(KERMIT, 0x1021, 0xFFFF, true, true, 0x0, 0x0, 0x29b1);
+DECLARE_CRC16(IBM_3740, 0x1021, 0xFFFF, false, false, 0x0, 0x0, 0x29b1);
 
-/*
-void precompute_table(struct crc16 params) {
-    for(int i=0; i<0xFF; i++) {
-        uint16_t crc = i;
-        for(int j = 0; j < 8; j++) {
-            crc = (crc >> 1) ^ (-(uint16_t)(crc & 1) & params.poly);
-        }
-        table[i] = crc;
-    }
-}
+DECLARE_CRC16(KERMIT, 0x1021, 0x0000, true, true, 0x0, 0x0, 0x2189);
 
-uint16_t compute_crc(char* buf, size_t len, struct crc16 params)
-{
-    uint16_t crc = params.init;
-    char *current = buf;
-    while(len--) {
-        crc = (crc >> 8) ^ table[(crc ^ *current++) & 0xFF];
-    }
-    return crc;
-} */
+#define DECLARE_TEST(NAME, BUF, VAR) \
+    VAR = compute_crc(BUF, 9, CRC16_##NAME); \
+    printf("%s %s: 0x%.4x\n", xstr(CRC16_##NAME),\
+        (result == CRC16_##NAME.check)?"PASSED":"FAILED", VAR)
 
 uint64_t reflect(uint64_t val, unsigned count)
 {
@@ -59,65 +46,6 @@ uint64_t reflect(uint64_t val, unsigned count)
     return ret;
 }
 
-#ifdef EVIL
-uint16_t compute_crc(const char* buf, size_t len, struct crc16 params) {
-	uint16_t crc;
-	const char *ptr;
-	size_t a;
-
-	crc = params.init;
-	ptr = buf;
-
-	if ( ptr != NULL ) for (a=0; a<len; a++) {
-
-		crc = (crc << 8) ^ table[ ((crc >> 8) ^ (uint16_t) *ptr++) & 0x00FF ];
-	}
-
-	return crc;
-
-}
-
-void precompute_table(struct crc16 params) {
-
-	uint16_t i;
-	uint16_t j;
-	uint16_t crc;
-	uint16_t c;
-
-	for (i=0; i<256; i++) {
-
-		crc = 0;
-		c   = i << 8;
-
-		for (j=0; j<8; j++) {
-
-			if ( (crc ^ c) & 0x8000 ) crc = ( crc << 1 ) ^ params.poly;
-			else                      crc =   crc << 1;
-
-			c = c << 1;
-		}
-
-		table[i] = crc;
-	}
-
-} 
-#else
-uint16_t compute_crc(const char* buf, size_t len, struct crc16 params) {
-	uint16_t crc;
-	const unsigned char *ptr;
-	size_t a;
-
-	crc = params.init;
-	ptr = buf;
-
-	if ( ptr != NULL ) for (a=0; a<len; a++) {
-
-		crc = (crc << 8) ^ table[ ((crc >> 8) ^ (uint16_t) *ptr++) & 0x00FF ];
-	}
-
-	return crc;
-
-}
 void precompute_table(struct crc16 params) {
     uint16_t i;
 	uint16_t j;
@@ -141,53 +69,31 @@ void precompute_table(struct crc16 params) {
 	}
 
 }
-#endif
-/*
-uint16_t compute_crc(char* buf, size_t len, struct crc16 params) {
+uint16_t compute_crc(const char* buf, size_t len, struct crc16 params) {
 	uint16_t crc;
 	const char *ptr;
 	size_t a;
+
+    precompute_table(params);
 
 	crc = params.init;
 	ptr = buf;
 
 	if ( ptr != NULL ) for (a=0; a<len; a++) {
 
-		crc = (crc >> 8) ^ table[ (crc ^ (uint16_t) *ptr++) & 0x00FF ];
+		crc = (crc << 8) ^ table[ ((crc >> 8) ^ (uint16_t) *ptr++) & 0x00FF ];
 	}
 
 	return crc;
 
 }
 
-void precompute_table(struct crc16 params) {
-
-	uint16_t i;
-	uint16_t j;
-	uint16_t crc;
-	uint16_t c;
-
-	for (i=0; i<256; i++) {
-
-		crc = 0;
-		c   = i;
-
-		for (j=0; j<8; j++) {
-
-			if ( (crc ^ c) & 0x0001 ) crc = ( crc >> 1 ) ^ params.poly;
-			else                      crc =   crc >> 1;
-			c = c >> 1;
-		}
-		table[i] = crc;
-	}
-}*/
-
 int main(int argc, char**argv) {
     uint16_t result = 0;
     char buf[] = "123456789";
 
-    printf("hello, world!\n");
-    result = compute_crc(buf, 9, CRC16_KERMIT);
-    printf("CRC Kermit %s: 0x%.4x\n", (0x29b1 == CRC16_KERMIT.check)?"PASSED":"FAILED", result);
+   DECLARE_TEST(IBM_3740, buf, result);
+   DECLARE_TEST(KERMIT, buf, result);
+
     return 0;
 }
