210487e097642c7ac7d109b51250cdd2dc7d3424
Jordan Montgomery <montytyper@msn.com>
2021-03-24 17:06:05 -0700

    Simplified table gen code.

--- main.c
+++ main.c
@@ -12,27 +12,30 @@ struct crc16 {
 	uint16_t xor_out;
 	uint16_t residue;
     uint16_t check;
+    uint16_t width;
 };
 
 #define xstr(a) str(a)
 #define str(a) #a
 #define BITMASK(X) (1L << (X))
 #define CRC16_BITS (16)
-#define DECLARE_CRC16(NAME, POLY, INIT, REF_IN, REF_OUT, XOR_OUT, RESIDUE, CHECK)   \
-    struct crc16 CRC16_##NAME = (struct crc16){.poly = POLY, .init = INIT, \
+#define DECLARE_CRC(NAME, POLY, INIT, REF_IN, REF_OUT, XOR_OUT, RESIDUE, CHECK, WIDTH)   \
+    struct crc16 CRC_##NAME = (struct crc16){.poly = POLY, .init = INIT, \
                                    .ref_in = REF_IN, .ref_out = REF_OUT,     \
-                                   .xor_out = XOR_OUT, .residue = RESIDUE, .check = CHECK }
+                                   .xor_out = XOR_OUT, .residue = RESIDUE, \
+                                   .check = CHECK, .width = WIDTH}
 
-DECLARE_CRC16(IBM_3740, 0x1021, 0xFFFF, false, false, 0x0, 0x0, 0x29b1);
-DECLARE_CRC16(KERMIT, 0x1021, 0x0000, true, true, 0x0, 0x0, 0x2189);
-DECLARE_CRC16(ISO_14443_3_A, 0x1021, 0xc6c6, true, true, 0x0, 0x0, 0xbf05);
-DECLARE_CRC16(MAXIM_DOW, 0x8005, 0x0000, true, true, 0xffff, 0x0, 0x44c2);
-DECLARE_CRC16(USB, 0x8005, 0xffff, true, true, 0xffff, 0x0, 0xb4c8);
+DECLARE_CRC(IBM_3740, 0x1021, 0xFFFF, false, false, 0x0, 0x0, 0x29b1, 16);
+DECLARE_CRC(KERMIT, 0x1021, 0x0000, true, true, 0x0, 0x0, 0x2189, 16);
+DECLARE_CRC(ISO_14443_3_A, 0x1021, 0xc6c6, true, true, 0x0, 0x0, 0xbf05, 16);
+DECLARE_CRC(MAXIM_DOW, 0x8005, 0x0000, true, true, 0xffff, 0x0, 0x44c2, 16);
+DECLARE_CRC(USB, 0x8005, 0xffff, true, true, 0xffff, 0x0, 0xb4c8, 16);
+DECLARE_CRC(CDMA2000, 0x2f, 0x1f, false, false, 0xff, 0x0, 0xdf, 8);
 
 #define DECLARE_TEST(NAME, BUF, VAR) \
-    VAR = compute_crc(BUF, 9, CRC16_##NAME); \
-    printf("%s %s: 0x%.4x\n", xstr(CRC16_##NAME),\
-        (result == CRC16_##NAME.check)?"PASSED":"FAILED", VAR)
+    VAR = compute_crc(BUF, 9, CRC_##NAME); \
+    printf("%s %s: 0x%.4x\n", xstr(CRC_##NAME),\
+        (result == CRC_##NAME.check)?"PASSED":"FAILED", VAR)
 
 uint64_t reflect(uint64_t val, unsigned count)
 {
@@ -48,29 +51,25 @@ uint64_t reflect(uint64_t val, unsigned count)
     return ret;
 }
 
-void precompute_table(struct crc16 params) {
-    uint16_t i;
-	uint16_t j;
-	uint16_t crc;
-	uint16_t c;
+void precompute_table(struct crc16 params)
+{
+    uint16_t mask = 1 << (params.width - 1);
+    for (int byte = 0; byte < 255; ++byte)
+    {
+        uint16_t crc = byte;
 
-	for (i=0; i<255; i++) {
+        for (int bit = params.width; bit > 0; --bit)
+        {
+            if (crc & mask)
+                crc = (crc << 1) ^ params.poly;
+            else
+                crc <<= 1;
+        }
 
-		crc = 0;
-		c   = i << 8;
-
-		for (j=0; j<8; j++) {
-
-			if ( (crc ^ c) & 0x8000 ) crc = ( crc << 1 ) ^ params.poly;
-			else                      crc =   crc << 1;
-
-			c = c << 1;
-		}
-
-		table[i] = crc;
-	}
-
+        table[byte] = crc;
+    }
 }
+
 uint16_t compute_crc(const char* buf, size_t len, struct crc16 params) {
 	uint16_t crc;
 	const char *ptr;
@@ -91,10 +90,11 @@ uint16_t compute_crc(const char* buf, size_t len, struct crc16 params) {
     }
 
     if(params.ref_out)
-        return reflect(crc,16) ^ params.xor_out;
+        crc = reflect(crc,params.width) ^ params.xor_out;
     else
-	    return crc ^ params.xor_out;
+	    crc = crc ^ params.xor_out;
 
+    return crc;
 }
 
 int main(int argc, char**argv) {
@@ -107,6 +107,10 @@ int main(int argc, char**argv) {
     DECLARE_TEST(ISO_14443_3_A, buf, result);
     DECLARE_TEST(MAXIM_DOW, buf, result);
     DECLARE_TEST(USB, buf, result);
+
+    DECLARE_TEST(CDMA2000, buf, result);
+
+    printf("0x%x 0x%x 0x%x 0x%x\n", table[0], table[1], table[2], table[3]);
 
     return 0;
 }
