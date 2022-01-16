a21554a4d53350e697786c2b0267aa65efb8faaa
Jordan M <montytyper@msn.com>
2022-01-15 17:34:58 -0800

    Update the crc_utility functions for easier verification
    These functions used larger counter types than strictly necessary. These have
    been reduced to types of more appropriate sizes. Additionally, it was
    discovered that a signed loop counter was declared and compared against an
    unsigned end condition. This mismatch was fixed, though it would not have
    resulted in incorrect behavior on any remotely sane system as used.

--- include/internal/crc_utility.h
+++ include/internal/crc_utility.h
@@ -1,6 +1,6 @@
 #ifndef SIMPLECRC_UTILITY_H_
 #define SIMPLECRC_UTILITY_H_
 
-uint64_t reflect(uint64_t val, unsigned count);
-uint64_t gen_mask(uint64_t width);
+uint64_t reflect(uint64_t val, uint8_t count);
+uint64_t gen_mask(uint8_t width);
 #endif /* SIMPLECRC_UTILITY_H_ */
--- src/crc_utility.c
+++ src/crc_utility.c
@@ -1,10 +1,10 @@
 #include "simplecrc.h"
 #include "internal/crc_utility.h"
 
-uint64_t reflect(uint64_t val, unsigned count)
+uint64_t reflect(uint64_t val, uint8_t count)
 {
 	uint64_t ret = val;
-	for (int i = 0; i < count; i++) {
+	for (uint8_t i = 0; i < count; i++) {
 		uint64_t srcbit = ((uint64_t)1 << i);
 		uint64_t dstbit = ((uint64_t)1 << (count - i - 1));
 		if ((val & srcbit) != 0)
@@ -15,7 +15,7 @@ uint64_t reflect(uint64_t val, unsigned count)
 	return ret;
 }
 
-uint64_t gen_mask(uint64_t width)
+uint64_t gen_mask(uint8_t width)
 {
 	if (width >= 64)
 		return 0xffffffffffffffff;
